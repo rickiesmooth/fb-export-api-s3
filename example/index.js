3ccda83505eb6022233039594bd978844a45e087
rick smit <rick@vigour.io>
2016-11-03 22:34:03 +0100

    refactored to use class

--- example/index.js
+++ example/index.js
@@ -6,5 +6,5 @@ const bucket = process.env.BUCKET_ID
 const fbS3 = require('../lib')
 
 fbS3(id, secret, bucket, { end: date, start: date - 86400 })
-  .then((data) => console.log('uploaded'))
+  .then((data) => console.log('yes'))
   .catch((err) => console.log(err))
