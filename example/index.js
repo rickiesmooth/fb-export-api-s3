585d4c9ddc87f6104a30c0d6a6a7424441ff2c1f
rick smit <rick@vigour.io>
2016-11-04 01:44:57 +0100

    update

--- example/index.js
+++ example/index.js
@@ -5,6 +5,9 @@ const secret = process.env.APP_SECRET
 const bucket = process.env.BUCKET_ID
 const fbS3 = require('../lib')
 
-fbS3(id, secret, bucket, { end: date, start: date - 86400 })
-  .then((data) => console.log('yes'))
-  .catch((err) => console.log(err))
+for (var service in body) {
+  console.log(body[service][1])
+  fbS3(body[service][0], body[service][1], bucket, { end: date, start: date - 86400 })
+    .then((data) => console.log('yes'))
+    .catch((err) => console.log(err))
+}
