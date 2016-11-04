f2282c3b74534c0b830f7e502b444a145425bafe
rick smit <rick@vigour.io>
2016-11-04 21:43:27 +0100

    added dep

--- example/index.js
+++ example/index.js
@@ -3,11 +3,8 @@ const date = ~~(Date.now() / 1000)
 const id = process.env.APP_ID
 const secret = process.env.APP_SECRET
 const bucket = process.env.BUCKET_ID
-const fbS3 = require('../lib')
+const upload = require('../lib')
 
-for (var service in body) {
-  console.log(body[service][1])
-  fbS3(body[service][0], body[service][1], bucket, { end: date, start: date - 86400 })
-    .then((data) => console.log('yes'))
-    .catch((err) => console.log(err))
-}
+upload(id, secret, bucket, { end: date, start: date - 86400 })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
